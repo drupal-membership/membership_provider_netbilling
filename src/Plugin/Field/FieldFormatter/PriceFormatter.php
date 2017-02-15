f04a18d581a34fdef72436c64ae0450572484d89
Brad Jones <brad@jones.name>
2017-02-14 17:43:35 -0700

    Revisions to the price formatter to optionally just show button

--- src/Plugin/Field/FieldFormatter/PriceFormatter.php
+++ src/Plugin/Field/FieldFormatter/PriceFormatter.php
@@ -4,12 +4,14 @@ namespace Drupal\membership_provider_netbilling\Plugin\Field\FieldFormatter;
 
 use Drupal\commerce_price\NumberFormatterFactoryInterface;
 use Drupal\commerce_price\Plugin\Field\FieldFormatter\PriceDefaultFormatter;
+use Drupal\commerce_price\Plugin\Field\FieldType\PriceItem;
 use Drupal\commerce_price\RounderInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormBuilderInterface;
 use Drupal\Core\Form\FormState;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Language\LanguageInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -72,6 +74,7 @@ class PriceFormatter extends PriceDefaultFormatter {
    * @inheritDoc
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
+    // We don't know how to handle more than two values for price, just show them.
     if (count($items) > 2) {
       return parent::viewElements($items, $langcode);
     }
@@ -80,40 +83,86 @@ class PriceFormatter extends PriceDefaultFormatter {
       $currency_codes[] = $item->currency_code;
     }
     $currencies = $this->currencyStorage->loadMultiple($currency_codes);
-
     $formState = (new FormState())
       ->addBuildInfo('entity', $items->getEntity());
     $elements = [];
+    $delta = 0;
     foreach ($items as $delta => $item) {
+      /** @var PriceItem $item */
       $currency = $currencies[$item->currency_code];
       $price = $this->numberFormatter->formatCurrency($item->number, $currency);
       $label = '';
-      if (count($items) > 1) {
+      $rawPrice = $this->rounder->round($item->toPrice())->getNumber();
+      // Always add a price.
+      $formState->addBuildInfo('price', $rawPrice);
+      // Two values equate to recurring billing.
+      if (count($items) > 1 && !$item->toPrice()->isZero()) {
         $label = $delta === 0 ? $this->t('Initial') : $this->t('Recurring');
-        $rawPrice = $this->rounder->round($item->toPrice())->getNumber();
-        if ($delta === 0) {
-          $formState->addBuildInfo('price', $rawPrice);
-        }
-        else {
+        if ($delta === 1) {
           $formState->addBuildInfo('recurring', $rawPrice);
         }
         $label .= ': ';
       }
-      $elements[$delta] = [
-        '#markup' => $label . $price,
-        '#cache' => [
-          'contexts' => [
-            'languages:' . LanguageInterface::TYPE_INTERFACE,
-            'country',
+      if (!$item->toPrice()->isZero()) {
+        $elements[$delta] = [
+          '#prefix' => $label,
+          '#markup' => $price,
+          '#cache' => [
+            'contexts' => [
+              'languages:' . LanguageInterface::TYPE_INTERFACE,
+              'country',
+            ],
           ],
-        ],
-      ];
+        ];
+      }
+    }
+    if (count($elements) === 1) {
+      unset($elements[0]['#prefix']);
+    }
+    // Return empty set if there was no price specified; nothing to sell.
+    if (!$elements) {
+      return $elements;
     }
 
-    if (!empty($delta)) {
-      $elements[$delta]['button'] = $this->formBuilder->buildForm('Drupal\membership_provider_netbilling\Form\BuyButtonForm', $formState);
+    $button = $this->formBuilder->buildForm('Drupal\membership_provider_netbilling\Form\BuyButtonForm', $formState);
+    if ($this->getSetting('only_buy_button')) {
+      $elements = [
+        ['button' => $button],
+      ];
+    }
+    else {
+      $elements[$delta + 1]['button'] = $button;
     }
     return $elements;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public static function defaultSettings() {
+    return [
+      'only_buy_button' => FALSE,
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $button = [
+      'only_buy_button' => [
+        '#type' => 'checkbox',
+        '#title' => $this->t('Show only the buy button, not the price values.'),
+        '#default_value' => $this->getSetting('only_buy_button'),
+      ],
+    ];
+    $upstream = parent::settingsForm($form, $form_state);
+    $upstream['strip_trailing_zeroes']['#states'] = $upstream['display_currency_code']['#states'] = [
+      'disabled' => [
+        ':input[name="fields[' . $form_state->getTriggeringElement()['#field_name'] . '][settings_edit_form][settings][only_buy_button]"]' => ['checked' => TRUE],
+      ],
+    ];
+    return $button + $upstream;
   }
 
 }
